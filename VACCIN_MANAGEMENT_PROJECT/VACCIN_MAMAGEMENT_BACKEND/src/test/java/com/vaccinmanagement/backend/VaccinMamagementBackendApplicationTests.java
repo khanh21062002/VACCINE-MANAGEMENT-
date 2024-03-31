7407bf54eef2830186a89a28d7b49f35cc0d0d3d
Khanh Robert <haduykhanh2106@gmail.com>
2024-03-31 20:51:43 +0700

    Add BACKEND AND FRONTEND folder

---
+++ VACCIN_MANAGEMENT_PROJECT/VACCIN_MAMAGEMENT_BACKEND/src/test/java/com/vaccinmanagement/backend/VaccinMamagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccinmanagement.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinMamagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
